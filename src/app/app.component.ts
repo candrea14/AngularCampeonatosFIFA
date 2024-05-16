bc385e357a8e681cf281770903b36480d14ce293
camilabv14 <camilabv14@hotmail.com>
2024-05-15 22:22:56 -0500

    Se agregan componentes de inicio y CRUD de selecciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReferenciasMaterialModule } from './shared/modulos/referencias-material.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReferenciasMaterialModule],
+  imports: [RouterOutlet, ReferenciasMaterialModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
